327b5ccdc466715d65b9be49dc95d956466a73f2
Anim <apaudel150@gmail.com>
2023-01-12 09:52:08 +0545

    Asim

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
+import Model.Result;
+import Model.Student;
+
+class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // create students
+        Student student1 = new Student("abs", 1, 85.5);
+        Student student2 = new Student("def", 2, 92.0);
+        Student student3 = new Student("ghi", 3, 75.0);
+
+        // create result object
+        Result result = new Result();
+
+        // add students to result
+        result.addStudent(student1);
+        result.addStudent(student2);
+        result.addStudent(student3);
+
+        // display result
+        result.displayResult();
     }
 }
---
+++ src/Model/Result.java
@@ -0,0 +1,27 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class Result {
+    private ArrayList<Student> students;
+
+    // constructor
+    public Result() {
+        students = new ArrayList<Student>();
+    }
+
+    // method to add student
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    // method to display result
+    public void displayResult() {
+        for (Student student : students) {
+            System.out.println("Name: " + student.getName());
+            System.out.println("Roll No: " + student.getRollNo());
+            System.out.println("Marks: " + student.getMarks());
+            System.out.println("-------------------");
+        }
+    }
+}
