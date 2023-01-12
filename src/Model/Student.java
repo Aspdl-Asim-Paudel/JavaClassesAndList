887ee023f2daf030efe964197b37cb0d617bed28
Anim <apaudel150@gmail.com>
2023-01-12 09:52:08 +0545

    Asim

---
+++ src/Model/Student.java
@@ -0,0 +1,41 @@
+package Model;
+
+import java.util.ArrayList;
+
+class Student {
+    private String name;
+    private int rollNo;
+    private double marks;
+
+    // constructor
+    public Student(String name, int rollNo, double marks) {
+        this.name = name;
+        this.rollNo = rollNo;
+        this.marks = marks;
+    }
+
+    // getter and setter methods
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public double getMarks() {
+        return marks;
+    }
+
+    public void setMarks(double marks) {
+        this.marks = marks;
+    }
+}
